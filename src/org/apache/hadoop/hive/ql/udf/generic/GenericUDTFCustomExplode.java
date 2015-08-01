bee723eb97f88d4cdedfa306bd115815b8646c14
Jove Kuang Desktop <cs.jove@gmail.com>
2015-07-31 23:46:47 -0700

    added first version of custom explode

---
+++ src/org/apache/hadoop/hive/ql/udf/generic/GenericUDTFCustomExplode.java
@@ -0,0 +1,85 @@
+package org.apache.hadoop.hive.ql.udf.generic;
+
+import org.apache.commons.lang.math.NumberUtils;
+import org.apache.hadoop.hive.ql.exec.Description;
+import org.apache.hadoop.hive.ql.exec.TaskExecutionException;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.serde2.objectinspector.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jovek
+ * Date: 7/31/15
+ * Time: 11:02 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Description(name = "custom_explode",
+        value = "_FUNC_(a) - explode a range of integers")
+public class GenericUDTFCustomExplode extends GenericUDTF {
+
+    private transient ObjectInspector inputOI = null;
+
+    @Override
+    public void close() throws HiveException {
+    }
+
+    @Override
+    public StructObjectInspector initialize(ObjectInspector[] args) throws UDFArgumentException {
+        if (args.length != 1) {
+            throw new UDFArgumentException("custom_explode() takes only one argument");
+        }
+
+        ArrayList<String> fieldNames = new ArrayList<String>();
+        ArrayList<ObjectInspector> fieldOIs = new ArrayList<ObjectInspector>();
+
+        switch (args[0].getCategory()) {
+            case LIST:
+                inputOI = args[0];
+                fieldNames.add("col");
+                fieldOIs.add(((ListObjectInspector)inputOI).getListElementObjectInspector());
+                break;
+            default:
+                throw new UDFArgumentException("custom_explode() takes an array of 3 elements as a parameter");
+        }
+
+        return ObjectInspectorFactory.getStandardStructObjectInspector(fieldNames,
+                fieldOIs);
+    }
+
+    private transient final Object[] forwardListObj = new Object[1];
+    private transient final Object[] forwardMapObj = new Object[2];
+
+    @Override
+    public void process(Object[] o) throws HiveException {
+        switch (inputOI.getCategory()) {
+            case LIST:
+                ListObjectInspector listOI = (ListObjectInspector)inputOI;
+                List<?> list = listOI.getList(o[0]);
+                if (list == null || list.size() != 3) {
+                    return; // we may want to throw an exception to let the user know that custom_explode takes an array of 3 elements
+                }
+                for (int newColumnValue = NumberUtils.toInt(list.get(0).toString());
+                       newColumnValue < NumberUtils.toInt(list.get(1).toString());
+                       newColumnValue+=NumberUtils.toInt(list.get(2).toString())) { // more error handling on invalid inputs
+                    forwardListObj[0] = list.get(0);
+                    forwardListObj[1] = list.get(1);
+                    forwardListObj[2] = newColumnValue;
+                    forward(forwardMapObj);
+                }
+                break;
+            default:
+                throw new TaskExecutionException("custom_explode() takes an array of 3 elements as a parameter");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "custom_explode";
+    }
+}
