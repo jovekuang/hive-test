a80ec690e14f47915c16785e5b481cf1b175c1c0
Jove Kuang Desktop <cs.jove@gmail.com>
2015-08-05 22:14:26 -0700

    updated to take 3 parameters and error handling.

--- src/com/jove/ql/udf/generic/GenericUDTFCustomExplode.java
+++ src/com/jove/ql/udf/generic/GenericUDTFCustomExplode.java
@@ -7,11 +7,11 @@ import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
 import org.apache.hadoop.hive.ql.udf.generic.GenericUDTF;
 import org.apache.hadoop.hive.serde2.objectinspector.*;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
 import org.apache.hadoop.io.Text;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -25,7 +25,11 @@ import java.util.Map;
         value = "_FUNC_(a) - explode a range of integers")
 public class GenericUDTFCustomExplode extends GenericUDTF {
 
-    private transient ObjectInspector inputOI = null;
+    private transient PrimitiveObjectInspector startOI = null;
+
+    private transient PrimitiveObjectInspector endOI = null;
+
+    private transient PrimitiveObjectInspector incrementOI = null;
 
     @Override
     public void close() throws HiveException {
@@ -33,26 +37,31 @@ public class GenericUDTFCustomExplode extends GenericUDTF {
 
     @Override
     public StructObjectInspector initialize(ObjectInspector[] args) throws UDFArgumentException {
-        if (args.length != 1) {
-            throw new UDFArgumentException("custom_explode() takes only one argument");
+        if (args.length != 3) {
+            throw new UDFArgumentException("custom_explode() takes 3 parameters");
         }
 
         ArrayList<String> fieldNames = new ArrayList<String>();
         ArrayList<ObjectInspector> fieldOIs = new ArrayList<ObjectInspector>();
 
-        switch (args[0].getCategory()) {
-            case LIST:
-                inputOI = args[0];
-                fieldNames.add("col1");
-                fieldNames.add("col2");
-                fieldNames.add("col3");
-                fieldOIs.add(((ListObjectInspector)inputOI).getListElementObjectInspector());
-                fieldOIs.add(((ListObjectInspector)inputOI).getListElementObjectInspector());
-                fieldOIs.add(((ListObjectInspector)inputOI).getListElementObjectInspector());
-                break;
-            default:
-                throw new UDFArgumentException("custom_explode() takes an array of 3 elements as a parameter");
+        for(int i = 0; i < args.length; i++) {
+            if (args[i].getCategory() != ObjectInspector.Category.PRIMITIVE
+                    && ((PrimitiveObjectInspector) args[0]).getPrimitiveCategory() != PrimitiveObjectInspector.PrimitiveCategory.INT) {
+                throw new UDFArgumentException("custom_explode() takes 3 positive integers as parameters");
+            }
         }
+
+        // input inspectors
+        startOI = (PrimitiveObjectInspector) args[0];
+        endOI = (PrimitiveObjectInspector) args[1];
+        incrementOI = (PrimitiveObjectInspector) args[2];
+
+        fieldNames.add("col1");
+        fieldNames.add("col2");
+        fieldNames.add("result_col");
+        fieldOIs.add(PrimitiveObjectInspectorFactory.javaIntObjectInspector);
+        fieldOIs.add(PrimitiveObjectInspectorFactory.javaIntObjectInspector);
+        fieldOIs.add(PrimitiveObjectInspectorFactory.javaIntObjectInspector);
 
         return ObjectInspectorFactory.getStandardStructObjectInspector(fieldNames,
                 fieldOIs);
@@ -62,24 +71,36 @@ public class GenericUDTFCustomExplode extends GenericUDTF {
 
     @Override
     public void process(Object[] o) throws HiveException {
-        switch (inputOI.getCategory()) {
-            case LIST:
-                ListObjectInspector listOI = (ListObjectInspector)inputOI;
-                List<?> list = listOI.getList(o[0]);
-                if (list == null || list.size() != 3) {
-                    return; // we may want to throw an exception to let the user know that custom_explode takes an array of 3 elements
-                }
-                for (int newColumnValue = NumberUtils.toInt(list.get(0).toString());
-                       newColumnValue <= NumberUtils.toInt(list.get(1).toString());
-                       newColumnValue+=NumberUtils.toInt(list.get(2).toString())) { // more error handling on invalid inputs
-                    forwardListObj[0] = list.get(0); // first column
-                    forwardListObj[1] = list.get(1); // second column
-                    forwardListObj[2] = new Text(Integer.toString(newColumnValue)); // third (new) column
+        if(o == null || o.length != 3){
+            throw new TaskExecutionException("custom_explode() takes 3 positive integers as parameters");
+        }
+        else {
+            Integer start = 0;
+            Integer end = 0;
+            Integer increment = 0;
+
+            try {
+                start = (Integer)startOI.getPrimitiveJavaObject(o[0]);
+                end = (Integer)endOI.getPrimitiveJavaObject(o[1]);
+                increment = (Integer)incrementOI.getPrimitiveJavaObject(o[2]);
+            } catch (ClassCastException e) {
+                throw new TaskExecutionException("custom_explode() takes 3 positive integers as parameters");
+            }
+
+            if(start > end || start < 0 || end < 0 || increment < 0) {
+                throw new TaskExecutionException("custom_explode() takes 3 positive integers as parameters and the first parameter must be less than the second parameter."
+                        +start+":"+end+":"+increment+" doesn't meet the criteria");
+            }
+            else {
+                for (int newColumnValue = start;
+                     newColumnValue <= end;
+                     newColumnValue+=increment) { // more error handling on invalid inputs
+                    forwardListObj[0] = start; // first column
+                    forwardListObj[1] = end; // second column
+                    forwardListObj[2] = newColumnValue; // third (new) column
                     forward(forwardListObj);
                 }
-                break;
-            default:
-                throw new TaskExecutionException("custom_explode() takes an array of 3 elements as a parameter");
+            }
         }
     }
 
